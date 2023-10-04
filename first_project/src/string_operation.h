a84ac88a39c98e62b5088df4091aac3bf214be5a
quitran1303 <quitran1303@gmail.com>
2023-10-05 08:58:27 +1100

    add changes for new structure

--- first_project/src/string_operation.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Qui Tran on 10/4/23.
-//
-
-#ifndef STRING_OPERATION_H
-#define STRING_OPERATION_H
-char * string_concat(char * str1, char * str2);
-int string_compare(char * str1, char * str2);
-#endif //STRING_OPERATION_H
